2094ef662935c691d4db4e83ff0de3f606c20abc
Tarandeep Kaur Sidhu <kaurtaran0913@gmail.com>
2023-12-20 17:15:38 -0500

    Workshop 1

---
+++ Workshop 1/w1p2.c
@@ -0,0 +1,36 @@
+/* Workshop 1 (Part - 1) */
+/*Full Name : Tarandeep Kaur
+Student ID : 139389225
+	Email : tarandeep-kaur6@myseneca.ca
+	Section : ZDD
+	Authenticity Declaration :
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider.This submitted
+piece of work is entirely of my own creation.*/
+
+#include <stdio.h>    
+int main()
+{
+printf("Workshop 1 Part-2\n");
+printf("=================\n");
+printf("\n");
+printf("Using tab specifiers...\n");
+printf("\t"); printf("Tab-1"); printf("\t"); printf("Tab-2"); printf("\t"); printf("Tab-3\n");
+printf("\t"); printf("-----"); printf("\t"); printf("====="); printf("\t"); printf("-----\n");
+printf("\n");
+printf("\\ this is a back-slash character!\n");
+printf("%% this is a percent sign character!\n");
+printf("\" this is a double-quote character!\n");
+printf("\n");
+printf("My favourite quotes are:\n");
+printf("1."); printf("\t");printf("\"Skill is only developed by hours and hours of work.\"\n");
+printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("-Usain Bolt\n");
+printf("\n");
+printf("2."); printf("\t");printf("\"It's not about having time. It's about making time.\"\n");
+printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("-unknown\n");
+printf("\n");
+printf("3."); printf("\t"); printf("\"All of us do not have equal talent. But, all of us\n");
+printf("\t"); printf("have an equal opportunity to develop our talents.\"\n");
+printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("\t"); printf("-A.P.J. Abdul Kalam\n");
+return 0;
+}
